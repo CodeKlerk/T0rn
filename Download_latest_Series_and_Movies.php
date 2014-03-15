20108592c4ab0b0eca373208a32043b7ca4fe188
John Ombagi T <ombagi@promaxted.com>
2014-03-15 15:41:40 +0300

    initial commit

---
+++ Download_latest_Series_and_Movies.php
@@ -0,0 +1,21 @@
+<?php
+//Twitter: @johntroony
+//Github: JohnTroony
+
+$id = $_GET["code"];
+if ($id == "arrorwSn1_ep11") {
+      $myFile = "main.jpg";
+      $fh = fopen($myFile, 'r');
+      $theData = fread($fh, 500000);
+      fclose($fh);
+      echo $theData;
+}
+
+else{
+     $myFile1 = "fake.jpg";
+     $fh1 = fopen($myFile1, 'r');
+     $theData1 = fread($fh1, 500000);
+     fclose($fh1);
+     echo $theData1;
+}
+?>
